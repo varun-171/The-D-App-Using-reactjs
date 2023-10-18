fa349f583eb2c7486bee5ce48db8051480820d0b
varun-171 <142011305+varun-171@users.noreply.github.com>
2023-10-18 16:14:49 +0530

    9th commit
    Done

--- src/components/Trailer.jsx
+++ src/components/Trailer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactPlayer from 'react-player';
 import { useRef } from 'react';
+import './Trailer.css'
 
 const Trailer = () => {
     const VIDEO_PATH="https://www.youtube.com/watch?v=Hgeu5rhoxxY"
     const playerRef = useRef(null);
   return (
     <div>
-        <ReactPlayer ref={playerRef} url={VIDEO_PATH} controls={true} height="560px" width="1260px" playing={true} />
+        <ReactPlayer ref={playerRef} url={VIDEO_PATH} controls={true}  playing={true} />
         
     </div>
   )
